b5c6cd9e4bd58dca26b295c80ebbc51aceb5177a
DaijobuDes <git@daijobudes.net>
2025-10-18 01:56:15 +0800

    fix: fast registering of commands by looping each guild list

--- src/register.ts
+++ src/register.ts
@@ -1,7 +1,8 @@
-import { REST, Routes } from 'discord.js';
+import { Client, GatewayIntentBits, REST, Routes } from 'discord.js';
 import dotenv from 'dotenv';
 import fs from 'node:fs';
 import path from 'node:path';
+import { log } from './logger';
 
 dotenv.config();
 
@@ -9,7 +10,7 @@ const token = process.env.DISCORD_BOT_TOKEN ?? '';
 const clientId = process.env.DISCORD_CLIENT_ID ?? '';
 const guildId = process.env.DISCORD_GUILD_ID ?? '';
 
-const commands = [];
+let commands = [];
 // Grab all the command folders from the commands directory you created earlier
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
@@ -27,34 +28,43 @@ for (const folder of commandFolders) {
     if ('data' in command && 'execute' in command) {
       commands.push(command.data.toJSON());
     } else {
-      console.log(
-        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+      log.warning(
+        `The command at ${filePath} is missing a required "data" or "execute" property.`
       );
     }
   }
 }
 
 // Construct and prepare an instance of the REST module
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent],
+});
 const rest = new REST().setToken(token);
 
 // and deploy your commands!
-(async () => {
-  try {
-    console.log(
-      `Started refreshing ${commands.length} application (/) commands.`
-    );
+client.once('ready', async () => {
+  const guilds = client.guilds.cache.map(guild => guild.id);
+  console.log(guilds);
+  for (const guildId of guilds) {
+    try {
+      log.info(
+        `Started refreshing ${commands.length} application (/) commands.`
+      );
 
-    // The put method is used to fully refresh all commands in the guild with the current set
-    const data: any = await rest.put(
-      Routes.applicationGuildCommands(clientId, guildId),
-      { body: commands }
-    );
+      // The put method is used to fully refresh all commands in the guild with the current set
+      const data: any = await rest.put(
+        Routes.applicationGuildCommands(clientId, guildId),
+        { body: commands }
+      );
 
-    console.log(
-      `Successfully reloaded ${data.length} application (/) commands.`
-    );
-  } catch (error) {
-    // And of course, make sure you catch and log any errors!
-    console.error(error);
+      log.success(
+        `Successfully reloaded ${data.length} application (/) commands.`
+      );
+    } catch (error) {
+      // And of course, make sure you catch and log any errors!
+      console.error(error);
+    }
   }
-})();
+});
+
+client.login(token);
